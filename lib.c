988124cfd90334592ad31cec88bf5398d17d58a4
Icaru-s <icaro2007ryan@gmail.com>
2026-04-01 21:20:07 -0300

    addMenu-SwitchCase+ReadDB

--- lib.c
+++ lib.c
@@ -3,29 +3,54 @@
 struct book{
         int id;
         char name[50];
-        char gender[50];
+        char genre[50];
     };
 
 int main(){
    
-    FILE *db = fopen("db.txt", "a");  // Pointer
 
-    if (db != NULL){     //checks if the database exists
-        struct book nBook;
-        printf("Please provide the ID of the book to be added: ");
-        scanf(" %d", &nBook.id);
-        printf("Please provide the NAME of the book to be added: ");
-        scanf(" %[^\n]s", nBook.name);
-        printf("Please provide the GENDER of the book to be added: ");
-        scanf(" %[^\n]s", nBook.gender);
+    int escolha;
 
-        fprintf(db, "%d|%s|%s\n", nBook.id, nBook.name, nBook.gender);
-        fflush(db);
-        fclose(db);
-        printf("File SAVED!");
+    do{
+        printf("---MENU---\n");
+        printf("Choose the option:\n");
+        printf("1- Add Book\n");
+        printf("2- Read Lib\n");
+        printf("3- Exclude\n");
+        printf("4- Quit\n");
+        scanf("%d", &escolha);
 
-    }else{
-        printf("The file could not be opened!");
-    }
+        switch (escolha){
+            case 1:
+                FILE *db = fopen("db.txt", "a");  // Pointer
+                if (db != NULL){     //checks if the database exists
+                    struct book nBook;
+                    printf("Please provide the ID of the book to be added: ");
+                    scanf(" %d", &nBook.id);                
+                    printf("Please provide the NAME of the book to be added: ");
+                    scanf(" %[^\n]s", nBook.name);
+                    printf("Please provide the GENRE of the book to be added: ");
+                    scanf(" %[^\n]s", nBook.genre);                
+                    fprintf(db, "%d|%s|%s\n", nBook.id, nBook.name, nBook.genre);
+                    fflush(db);
+                    fclose(db);
+                    printf("File SAVED!");
+                }else{
+                    printf("The file could not be opened!");
+                }break;
+            case 2:
+                FILE *dbRead = fopen("db.txt", "r"); // OPEN archive to Read
+                if (dbRead == NULL){printf("Error: DataBase is empty!\n");} // Return error
+                else{
+                    struct book b; // Temp variable
+                    printf("-----LIBRARY-----\n");
 
+                    while(fscanf(dbRead, "%d|%[^|]|%[^\n]\n", b.id, b.name, b.genre) == 3){
+                        printf("ID: %d | NAME: %s | GENRE: %s\n", b.id, b.name, b.genre);
+                    }
+                    fclose(dbRead);
+                }break;
+            
+        }
+    }while(escolha != 4);
 }
